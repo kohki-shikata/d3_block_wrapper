d6f2df42af2f0b983ac55fbbe20ba34fc666adb3
kohki-shikata <indigoworks@gmail.com>
2014-07-22 11:40:10 +0900

    Add to display actual markup element

--- blocks/d3_block_wrapper/templates/close.php
+++ blocks/d3_block_wrapper/templates/close.php
@@ -4,7 +4,7 @@ defined('C5_EXECUTE') or die("Access Denied.");
 $c = Page::getCurrentPage();
 
 if($c->isEditMode() OR $c->isAdminArea() OR strstr($_SERVER['REQUEST_URI'], 'edit_block_popup')){
-	echo '<div style="height: 20px; width: 100%;" class="d3-block-wrapper-close">'.t('Close Wrapper').'</div>';
+	echo '<div style="height: 20px; width: 100%;" class="d3-block-wrapper-close">'.t('Close Wrapper (&lt;/div&gt;)').'</div>';
 } else {
 	echo '</div>';
 	echo '</div>';
