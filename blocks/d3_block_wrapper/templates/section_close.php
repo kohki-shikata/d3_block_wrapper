61fe249654f4c1a9e8977503c6cba441ab043612
kohki-shikata <indigoworks@gmail.com>
2014-07-22 11:38:19 +0900

    Create section_close.php

---
+++ blocks/d3_block_wrapper/templates/section_close.php
@@ -0,0 +1,11 @@
+<?php 
+defined('C5_EXECUTE') or die("Access Denied.");
+
+$c = Page::getCurrentPage();
+
+if($c->isEditMode() OR $c->isAdminArea() OR strstr($_SERVER['REQUEST_URI'], 'edit_block_popup')){
+	echo '<div style="height: 20px; width: 100%;" class="d3-block-wrapper-close">'.t('Close Wrapper (&lt;/section&gt;)').'</div>';
+} else {
+	echo '</div>';
+	echo '</section>';
+}
